19baff7358aedc64ece762062dd580c92b59f01f
d-kang <david.kang714@gmail.com>
2017-07-18 12:28:48 -0700

    Init Parse in Videos.js

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -10,6 +10,25 @@ var Videos = Backbone.Collection.extend({
 
   select: function() {
     this.trigger('select', this);
+  },
+
+  search: function(query) {
+    // var query = $('.form-control').val();
+    Backbone.ajax({
+      data: {q: query},
+      // url: "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=twitterapi&count=25",
+      // data: "",
+      // dataType: "json",
+      // contentType: JSON.stringify(data)
+      success: function success() { console.log('success') },
+      error: function (error) { console.log('failed') }
+    });
+  },
+
+  parse: function(data) {
+    // var response = collection.parse({ items: fakeVideoData });
+    // expect(response).to.be.equal(fakeVideoData);
+    return data.items;
   }
 
 });
